6fcecfd58beb20481c47efeda899728372f9b759
0xMayo <wakjiraalemayehu@gmail.com>
2024-05-06 14:30:24 +0200

    Defining the structure of the block

---
+++ models/Block.js
@@ -0,0 +1,18 @@
+export default class Block {
+    constructor(
+      timestamp,
+      blockIndex,
+      previousBlockHash,
+      currentBlockHash,
+      data,
+      difficulty
+    ) {
+      this.timestamp = timestamp;
+      this.blockIndex = blockIndex;
+      this.previousBlockHash = previousBlockHash;
+      this.currentBlockHash = currentBlockHash;
+      this.data = data;
+      this.difficulty = difficulty || process.env.DIFFICULTY_LEVEL;
+    }
+  }
+  
